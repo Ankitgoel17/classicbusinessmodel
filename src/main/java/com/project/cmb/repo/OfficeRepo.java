bf74a79252e24a92013dd2079b2c9a94995ff451
rolex1420 <surajitduary426@gmail.com>
2026-05-19 16:15:12 +0530

    Some Changes made in OfficeRepo

--- src/main/java/com/project/cmb/repo/OfficeRepo.java
+++ src/main/java/com/project/cmb/repo/OfficeRepo.java
@@ -1,15 +1,20 @@
 package com.project.cmb.repo;
 
 import com.project.cmb.entity.Office;
+import com.project.cmb.projection.OfficeDetailView;
 import com.project.cmb.projection.OfficeListView;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import java.util.Optional;
 
 @RepositoryRestResource(path = "office")
 public interface OfficeRepo extends JpaRepository<Office, String> {
-    List<OfficeListView> findByCountryIn(List<String> countries);
 
+    List<OfficeListView> findAllProjectedBy();
+
+    Optional<OfficeDetailView> findOfficeDetailByOfficeCode(String officeCode);
+
+    List<OfficeListView> findByCountryIn(List<String> countries);
 }
